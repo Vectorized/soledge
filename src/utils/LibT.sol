411bea3942d0738843a239203bb3bd49b8d456d3
rahul <raxhvl@users.noreply.github.com>
2024-02-02 23:13:55 +0530

    🥢: LibT comment updated  MCOPY -> TLOAD/TSTORE

--- src/utils/LibT.sol
+++ src/utils/LibT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 /// @notice Transient storage helper.
 /// @author Soledge (https://github.com/vectorized/soledge/blob/main/src/utils/LibT.sol)
 ///
-/// @dev Note: This implementation utilizes the `MCOPY` opcode.
+/// @dev Note: This implementation utilizes `TLOAD`, and `TSTORE` opcodes.
 /// Please ensure that the chain you are deploying on supports them.
 library LibT {
     /// @dev Returns the value at `tSlot` in transient storage.
